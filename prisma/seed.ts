4a9f5230b54628cb9c20bfa207d18cd6d8db9e56
Tom Harris <tomharris3@me.com>
2024-12-30 19:03:45 +0000

    optimize the seed function

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,19 +7,38 @@ async function main() {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
 
+  type Song = {
+    title: string;
+    artist: string;
+  };
+
   const songsData = JSON.parse(
     fs.readFileSync(path.resolve(__dirname, '../src/data/songs.json'), 'utf-8')
+  ) as Song[];
+
+  const existingSongs = prisma.song.findMany({
+    select: { title: true, artist: true },
+  }) as Promise<Song[]>;
+
+  const existingSongsSet = new Set(
+    (await existingSongs).map((song) => `${song.title}-${song.artist}`)
   );
 
-  for (const song of songsData) {
-    await prisma.song.upsert({
-      where: { artist_title: { title: song.songName, artist: song.artist } },
-      update: {},
-      create: {
-        title: song.songName,
+  const newSongs = songsData.filter(
+    (song) => !existingSongsSet.has(`${song.title}-${song.artist}`)
+  );
+
+  if (newSongs.length > 0) {
+    await prisma.song.createMany({
+      data: newSongs.map((song) => ({
+        title: song.title,
         artist: song.artist,
-      },
+      })),
+      skipDuplicates: true,
     });
+    console.log(`Added ${newSongs.length} new songs.`);
+  } else {
+    console.log('No new songs added');
   }
 
   console.log('Seeding completed');
