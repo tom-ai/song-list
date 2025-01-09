5dfafea5fcca649abf7d30a9143dcde07c6e6443
Tom Harris <tomharris3@me.com>
2025-01-09 16:59:47 +0000

    update hook with loading and error, get data from context

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,16 @@
 import SongList from '../components/SongList';
-import { useSearchParams } from 'react-router';
-import { useEffect, useState } from 'react';
+import { useOutletContext, useSearchParams } from 'react-router';
+import { useState } from 'react';
 import SearchBox from '../components/SearchBox';
 import { filterSongs } from '../utils/songHelpers';
 import { useDebouncedSetParams } from '../hooks/useDebouncedSetParams';
-import { createClient } from '@supabase/supabase-js';
 import { Song } from './../types';
 
-const supabase = createClient(
-  'https://yaqgpshbigfdzihldrgw.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhcWdwc2hiaWdmZHppaGxkcmd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY0MTk0NzgsImV4cCI6MjA1MTk5NTQ3OH0.xtkfhfC9OZJlZfc5L81TkTg8N9jC941b1_ll7B4hS74'
-);
+interface AppContext {
+  songs: Song[];
+  isLoading: boolean;
+  error: string | null;
+}
 
 export default function MainPage() {
   const [params, setParams] = useSearchParams({});
@@ -21,19 +21,9 @@ export default function MainPage() {
 
   const debouncedSetParams = useDebouncedSetParams(setParams, 500);
 
-  const [songs, setSongs] = useState<Song[]>([]);
+  const { songs, isLoading, error } = useOutletContext<AppContext>();
 
-  useEffect(() => {
-    getSongs();
-  }, []);
-
-  async function getSongs() {
-    const { data } = await supabase.from('Song').select();
-
-    if (data) {
-      setSongs(data);
-    }
-  }
+  const filteredSongs = songs.filter((song) => filterSongs(song, inputValue));
 
   return (
     <section>
@@ -44,12 +34,14 @@ export default function MainPage() {
           debouncedSetParams(value);
         }}
       />
-      {q !== '' && songs.length === 0 ? (
+      {isLoading ? (
+        <p>Loading songs...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : q !== '' && filteredSongs.length === 0 ? (
         <p>No songs found matching "{inputValue}"</p>
       ) : (
-        <SongList
-          songs={songs.filter((song) => filterSongs(song, inputValue))}
-        />
+        <SongList songs={filteredSongs} />
       )}
     </section>
   );
