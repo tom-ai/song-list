974152ca824a860b6ee290811090890acb196c45
Tom Harris <tomharris3@me.com>
2026-01-17 13:43:30 +0000

    remove unused files

--- src/api/song.ts
+++
@@ -1,9 +0,0 @@
-import { supabase } from '.';
-import { Song } from '../types';
-
-// use swr here, or just put in hook?
-export async function getSongs(): Promise<Song[]> {
-  const { data, error } = await supabase.from('Song').select();
-  if (error) throw new Error(`Failed to fetch songs - ${error.message}`);
-  return data ?? [];
-}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,4 @@
-// import type { Database } from './database.types';
-
-// export type Song = Database['public']['Tables']['Song']['Row'];
-// export type Playlist = Database['public']['Tables']['Playlist']['Row'];
-// export type PlaylistSong = Database['public']['Tables']['PlaylistSong']['Row'];
-
-// export type SongWithGenres = Song & {
-//   genres: { name: string }[];
-// };
+// Types imported from Groove App
 
 export type PublicSong = {
 	id: string
